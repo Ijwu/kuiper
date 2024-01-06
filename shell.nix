8fc6cbffab3334aecc926ddc1c7f33e2507fbdaf
Hussein Farran <hfarran@gmail.com>
2024-01-06 02:22:39 -0500

    add some nix shit

---
+++ shell.nix
@@ -0,0 +1,5 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [ dotnet-sdk_8 dotnet-aspnetcore_8];
+}
