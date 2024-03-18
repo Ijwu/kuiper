4f3e8e612c0dd0223bd9839dfef731ca0663d979
Hussein Farran <hfarran@gmail.com>
2024-03-18 14:55:11 -0400

    add bash to shell to accommodate my new vscode system config

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [ dotnet-sdk_8 dotnet-aspnetcore_8];
+    nativeBuildInputs = with pkgs.buildPackages; [ dotnet-sdk_8 dotnet-aspnetcore_8 bash];
 }
