02fd5bc93fbb09e3ec75dd3a8b33cc486d9f4ed0
Hussein Farran <hussein.farran@nexcortech.com>
2024-11-25 14:06:49 -0500

    add more tests

--- belters/ReceivingBayTests.cs
+++ belters/ReceivingBayTests.cs
@@ -26,6 +26,105 @@ public class ReceivingBayTests
         bay.StartReceiving();
 
         // Assert
+        Thread.Sleep(150);
         packetHandlerMock.Verify(phm => phm(new Packet[]{new Say("Test")}), Times.AtLeastOnce);
+    }
+
+    [Test]
+    public void AsyncHandler_RegisterAndCallSuccessfully()
+    {
+        // Arrange
+        ITalkToTheServer mockServerTalker = Mock.Of<ITalkToTheServer>(mst =>
+            mst.IsConnected == true &&
+            mst.ReceivePacketsAsync(It.IsAny<CancellationToken>()) == Task.FromResult<Packet[]>(new Packet[] { new Say("Test") })
+        );
+        ReceivingBay bay = new ReceivingBay(mockServerTalker);
+
+        // Act
+        var packetHandlerMock = new Mock<PacketsReceivedHandlerAsync>();
+        var handlerHandle = bay.OnPacketsReceived(packetHandlerMock.Object);
+        bay.StartReceiving();
+
+        // Assert
+        Thread.Sleep(150);
+        packetHandlerMock.Verify(phm => phm(new Packet[] { new Say("Test") }), Times.AtLeastOnce);
+    }
+
+    [Test]
+    public void SyncHandler_DisposeHandler_NoLongerCallsHandler()
+    {
+        // Arrange
+        ITalkToTheServer mockServerTalker = Mock.Of<ITalkToTheServer>(mst =>
+            mst.IsConnected == true &&
+            mst.ReceivePacketsAsync(It.IsAny<CancellationToken>()) == Task.FromResult<Packet[]>(new Packet[] { new Say("Test") })
+        );
+        ReceivingBay bay = new ReceivingBay(mockServerTalker);
+
+        // Act
+        var packetHandlerMock = new Mock<PacketsReceivedHandler>();
+        var handlerHandle = bay.OnPacketsReceived(packetHandlerMock.Object);
+        bay.StartReceiving();
+        handlerHandle.Dispose();
+
+        // Assert
+        Thread.Sleep(150);
+        packetHandlerMock.Verify(phm => phm(new Packet[] { new Say("Test") }), Times.Never);
+    }
+
+    [Test]
+    public void AsyncHandler_DisposeHandler_NoLongerCallsHandler()
+    {
+        // Arrange
+        ITalkToTheServer mockServerTalker = Mock.Of<ITalkToTheServer>(mst =>
+            mst.IsConnected == true &&
+            mst.ReceivePacketsAsync(It.IsAny<CancellationToken>()) == Task.FromResult<Packet[]>(new Packet[] { new Say("Test") })
+        );
+        ReceivingBay bay = new ReceivingBay(mockServerTalker);
+
+        // Act
+        var packetHandlerMock = new Mock<PacketsReceivedHandlerAsync>();
+        var handlerHandle = bay.OnPacketsReceived(packetHandlerMock.Object);
+        bay.StartReceiving();
+        handlerHandle.Dispose();
+
+        // Assert
+        Thread.Sleep(150);
+        packetHandlerMock.Verify(phm => phm(new Packet[] { new Say("Test") }), Times.Never);
+    }
+
+    [Test]
+    public void SyncHandler_ConcurrentRegistrations_RegisterAndCallSuccessfully()
+    {
+        // Arrange
+        ITalkToTheServer mockServerTalker = Mock.Of<ITalkToTheServer>(mst =>
+            mst.IsConnected == true &&
+            mst.ReceivePacketsAsync(It.IsAny<CancellationToken>()) == Task.FromResult<Packet[]>(new Packet[] { new Say("Test") })
+        );
+        ReceivingBay bay = new ReceivingBay(mockServerTalker);
+
+        // Act
+        List<Mock<PacketsReceivedHandler>> mocks = new();
+        List<Task<IDisposable>> registerTasks = new();
+        for (int i = 0; i < 50; i++)
+        {
+            registerTasks.Add(Task.Run(() =>
+            {
+                var packetHandlerMock = new Mock<PacketsReceivedHandler>();
+                mocks.Add(packetHandlerMock);
+
+                var handlerHandle = bay.OnPacketsReceived(packetHandlerMock.Object);
+                return Task.FromResult(handlerHandle);
+            }));
+        }
+        
+        Task.WaitAll(registerTasks.ToArray());
+        bay.StartReceiving();
+
+        // Assert
+        Thread.Sleep(150);
+        foreach (var mock in mocks)
+        {
+            mock.Verify(phm => phm(new Packet[] { new Say("Test") }), Times.AtLeastOnce);
+        }
     }
 }
