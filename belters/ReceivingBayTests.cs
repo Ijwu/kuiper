aded8299a74cb4ba7852acbddbcc09e75e9b5be5
Hussein Farran <hfarran@gmail.com>
2024-11-25 02:02:59 -0500

    Update test proj deps and add tests for receiving bay

---
+++ belters/ReceivingBayTests.cs
@@ -0,0 +1,31 @@
+﻿using kbo.bigrocks;
+using kbo.littlerocks;
+
+using Moq;
+
+using spaceport;
+using spaceport.schematics;
+
+namespace belters;
+
+public class ReceivingBayTests
+{
+    [Test]
+    public void SyncHandler_RegisterAndCallSuccessfully()
+    {
+        // Arrange
+        ITalkToTheServer mockServerTalker = Mock.Of<ITalkToTheServer>( mst =>
+            mst.IsConnected == true &&
+            mst.ReceivePacketsAsync(It.IsAny<CancellationToken>()) == Task.FromResult<Packet[]>(new Packet[]{new Say("Test")})
+        );
+        ReceivingBay bay = new ReceivingBay(mockServerTalker);
+
+        // Act
+        var packetHandlerMock = new Mock<PacketsReceivedHandler>();
+        var handlerHandle = bay.OnPacketsReceived(packetHandlerMock.Object);
+        bay.StartReceiving();
+
+        // Assert
+        packetHandlerMock.Verify(phm => phm(new Packet[]{new Say("Test")}), Times.AtLeastOnce);
+    }
+}
