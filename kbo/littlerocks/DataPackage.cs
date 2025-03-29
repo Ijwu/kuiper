df32858346e1dd2ca31efb3b41b6cbf4ab9c0587
Hussein Farran <hmfarran@gmail.com>
2025-03-29 16:56:26 -0400

    packet review for `DataPackage`

--- kbo/littlerocks/DataPackage.cs
+++ kbo/littlerocks/DataPackage.cs
@@ -8,18 +8,14 @@ public record DataPackageGameData
     [JsonPropertyName("location_name_to_id")]
     public Dictionary<string, long> LocationNameToId { get; set; }
 
-    [JsonPropertyName("version")]
-    public int? Version { get; set; }
-
     [JsonPropertyName("checksum")]
     public string Checksum { get; set; }
 
-    public DataPackageGameData(Dictionary<string, long> itemNameToId, Dictionary<string, long> locationNameToId, string checksum, int? version = null)
+    public DataPackageGameData(Dictionary<string, long> itemNameToId, Dictionary<string, long> locationNameToId, string checksum)
     {
         ItemNameToId = itemNameToId;
         LocationNameToId = locationNameToId;
         Checksum = checksum;
-        Version = version;
     }
 }
 
