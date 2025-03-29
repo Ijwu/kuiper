371b49eda064a8d9791622541c348f7aa98f50d7
Hussein Farran <hmfarran@gmail.com>
2025-03-29 16:34:34 -0400

    replace roomupdate packet with to-do since I was doing it wrong

--- kbo/bigrocks/RoomUpdate.cs
+++ kbo/bigrocks/RoomUpdate.cs
@@ -2,16 +2,6 @@ namespace kbo.bigrocks;
 
 public record RoomUpdate : Packet
 {
-    [JsonPropertyName("players")]
-    public NetworkPlayer[] Players { get; set; }
-
-    [JsonPropertyName("checked_locations")]
-    public long[] CheckedLocations { get; set; }
-
-    public RoomUpdate(NetworkPlayer[] players, long[] checkedLocations)
-    {
-        Players = players;
-        CheckedLocations = checkedLocations;
-    }
+    //TODO: Re-do, old implementation was totally wrong.
 }
 
