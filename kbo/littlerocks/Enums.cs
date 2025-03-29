88288ba48dfe95ba3c9264a57cbed5b27a9e9565
Hussein Farran <hmfarran@gmail.com>
2025-03-29 16:56:14 -0400

    consolidate enums into single file

---
+++ kbo/littlerocks/Enums.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.Serialization;
+
+namespace kbo.littlerocks;
+
+public enum ClientStatus
+{
+    ClientUnknown = 0,
+    ClientConnected = 5,
+    ClientReady = 10,
+    ClientPlaying = 20,
+    ClientGoal = 30
+}
+
+public enum HintStatus
+{
+    Unspecified = 0,
+    NoPriority = 10,
+    Avoid = 20,
+    Priority = 30,
+    Found = 40
+}
+
+[Flags]
+public enum NetworkItemFlags
+{
+    None = 0b000,
+    Advancement = 0b001,
+    Useful = 0b010,
+    Trap = 0b100,
+}
+
+[Flags]
+public enum ItemHandlingFlags
+{
+    None = 0b000,
+    Remote = 0b001,
+    LocalAndRemote = 0b011,
+    StartingInventoryAndRemote = 0b101,
+    All = 0b111,
+}
+
+[Flags]
+public enum SlotType
+{
+    Spectator = 0b00,
+    Player = 0b01,
+    Group = 0b10,
+}
--- kbo/littlerocks/SlotType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace kbo.littlerocks;
-
-[Flags]
-public enum SlotType
-{
-    Spectator = 0b00,
-    Player = 0b01,
-    Group = 0b10,
-}
