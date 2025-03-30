9bfa6937db62a9e784158d91c3d82a398c0b814a
Hussein Farran <hmfarran@gmail.com>
2025-03-30 12:13:11 -0400

    Fix RoomUpdate packet based on what I could see in AP src

--- kbo/bigrocks/RoomUpdate.cs
+++ kbo/bigrocks/RoomUpdate.cs
@@ -2,6 +2,37 @@ namespace kbo.bigrocks;
 
 public record RoomUpdate : Packet
 {
-    //TODO: Re-do, old implementation was totally wrong.
+    [JsonPropertyName("hint_points")]
+    public long? HintPoints { get; set; }
+
+    [JsonPropertyName("players")]
+    public NetworkPlayer[]? Players { get; set; }
+
+    [JsonPropertyName("checked_locations")]
+    public long[]? CheckedLocations { get; set; }
+
+    [JsonPropertyName("hint_cost")]
+    public long? HintCost { get; set; }
+
+    [JsonPropertyName("location_check_points")]
+    public long? LocationCheckPoints { get; set; }
+
+    [JsonPropertyName("permissions")]
+    public Dictionary<string, CommandPermission>? Permissions { get; set; }
+
+    public RoomUpdate(long? hintPoints,
+                      NetworkPlayer[]? players,
+                      long[]? checkedLocations,
+                      long? hintCost,
+                      long? locationCheckPoints,
+                      Dictionary<string, CommandPermission>? permissions)
+    {
+        HintPoints = hintPoints;
+        Players = players;
+        CheckedLocations = checkedLocations;
+        HintCost = hintCost;
+        LocationCheckPoints = locationCheckPoints;
+        Permissions = permissions;
+    }
 }
 
