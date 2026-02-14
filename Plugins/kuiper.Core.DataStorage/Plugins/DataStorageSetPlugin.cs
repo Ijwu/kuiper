8b4a22f3034646f2f63e6ac1fe513eb91cbf47b2
Hussein Farran <hmfarran@gmail.com>
2026-02-14 16:42:46 -0500

    Sync namespaces

--- Plugins/kuiper.Core.DataStorage/Plugins/DataStorageSetPlugin.cs
+++ Plugins/kuiper.Core.DataStorage/Plugins/DataStorageSetPlugin.cs
@@ -7,10 +7,11 @@ using kbo.littlerocks;
 
 using kuiper.Core.Constants;
 using kuiper.Core.Services.Abstract;
+using kuiper.Plugins;
 
 using Microsoft.Extensions.Logging;
 
-namespace kuiper.Plugins
+namespace kuiper.Core.DataStorage.Plugins
 {
     public class DataStorageSetPlugin : BasePlugin
     {
